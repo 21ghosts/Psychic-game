3e869084db3c58ecb6a2516afc67e07afeb2a8af
21ghosts <nathanmyoung44@gmai.com>
2018-02-21 11:36:56 -0500

    final commit with comments and intended text position

--- psychic.js
+++ psychic.js
@@ -11,7 +11,7 @@ var letters = ['a', 'b', 'c',
 var guessThusFar = [];
 var winCount = 0;
 var lossCount = 0;
-var guessCount = 5;
+var guessCount = 8;
 
 
 var letterToGuess = letters[Math.floor(Math.random() * letters.length)]; // selects random letter on screen load
@@ -19,31 +19,31 @@ var letterToGuess = letters[Math.floor(Math.random() * letters.length)]; // sele
 document.onkeypress = function game(event) 
 {
 
-    var userInput = String.fromCharCode(event.keyCode).toLowerCase();
+    var userInput = String.fromCharCode(event.keyCode).toLowerCase(); // this will store my userInput and push it to my guessThusFar array
     guessThusFar.push(userInput);
     
 
-    if(userInput === letterToGuess)
+    if(userInput === letterToGuess) // if the userInput is equall to letterToguess, winCount will increment by 1, guessCount will return to 8, and guessThusFar will return to empty
     {
         winCount++;
-        guessCount = 5;
+        guessCount = 8;
         guessThusFar = [];
     }
-   else if(guessCount === 0)
+   else if(guessCount === 0) // if guessCount becomes equal to 0 lossCount will be incremented by 1, guessCount will return to 8, and guessThusFar will retun to empty
    {
        lossCount++;
-       guessCount = 5;
+       guessCount = 8;
        guessThusFar = [];
    }
-   else if(userInput !== letterToGuess){
+   else if(userInput !== letterToGuess){ // if userInput should not equal letterToGuess guessCount will decrease by 1
        guessCount--;
    }
+   //this will display all changes and updates to intinded area in html
+    var html = "<h1 class='text-center'>The Psyhcic Game</h1>" + "<hr>" + "<p class='text-center'>Guess the letter:</p>" + "<br>" + 
+        "<p class='text-center'>Wins: " + winCount + "</p>" + "<br>" + "<p class='text-center'>Losses: " + lossCount + "</p>" + "<br>" +
+        "<p class='text-center'>Remaining guesses: " + guessCount + "</p>" + "<br>" +"<p class='text-center'>Guesses thus far: " + guessThusFar + "</p>";
 
-    var html = "<h1>The Psyhcic Game</h1>" + "<p>Guess the letter:</p>" + 
-        "<p>Wins: " + winCount + "</p>" + "<p>Losses: " + lossCount + "</p>" + 
-        "<p>Remaining guesses: " + guessCount + "</p>" + "<p>Guesses thus far: " + guessThusFar + "</p>";
-
-    document.querySelector("#content").innerHTML = html
+    document.querySelector("#content").innerHTML = html // dispays html to content id, in index.html
 }
 
 document.querySelector("#content").innerHTML = html
